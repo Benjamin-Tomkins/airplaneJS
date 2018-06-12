389dc5e034a86d255167fbdc595d0aab5667f6e9
Benjamin Tomkins <bentomkins@hotmail.com>
2018-06-12 19:54:50 +0100

    code cleanup

--- src/Plane.js
+++ src/Plane.js
@@ -1,10 +1,15 @@
 "use strict";
 
+
 function Plane(){}
+
+
 Plane.prototype.land = function (airport) {
     airport.clearForLanding(this);
     this._location = airport;
 }
+
+
 Plane.prototype.takeOff = function () {
     this._location.clearForTakeOff();
 };
